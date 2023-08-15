d75cb540e12e75b47cd04c9367f0d2ea6fd3c0a4
Tuyen Tran <tuyentt@tuyentt.com>
2023-08-15 18:39:12 +0700

    add

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -2,13 +2,27 @@ const httpStatus = require('http-status');
 const { User } = require('../models');
 const ApiError = require('../utils/ApiError');
 const Device = require('../models/device.model');
+const logger = require('../config/logger');
 
 
 const queryDevices = async () => {
- 
   return Device.find();
 };
 
+const createDevice = async (deviceBody) => {
+  logger.info('createDevice');
+  let device = await Device.findOne({SN: deviceBody.SN});
+  if(device){
+    // Update Devcie
+    device = Object.assign(device, deviceBody);
+    await device.save();
+  }
+  else
+    device = await Device.create(deviceBody);
+  return device;
+};
+
 module.exports = {
-  queryDevices
+  queryDevices,
+  createDevice
 };
